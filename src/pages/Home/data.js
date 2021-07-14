1fe348b071c511e2404b74add94caeb0cd200f80
Sudeepa Pal <palsudeepa@gmail.com>
2021-07-14 16:33:48 +0530

    everything has added

--- src/pages/Home/data.js
+++ src/pages/Home/data.js
@@ -1,4 +1,15 @@
 import abcd_img1 from "../../assets/abcd.jpg";
+import paaracetamol_img2 from "../../assets/paraceta.jpeg";
+import saridon_img3 from "../../assets/sari.jpg";
+import pd_img4 from "../../assets/pd.jpg";
+import vital_img5 from "../../assets/vital.jpg";
+import zanocin_img6 from "../../assets/zanocin.jpeg";
+import oniron_img7 from "../../assets/oniron.jpg";
+import cofsils_img8 from "../../assets/cough.jpg";
+import digene_img9 from "../../assets/digene.jpeg";
+import electral_img10 from "../../assets/electral.jpeg";
+import Alkasol_img11 from "../../assets/alka.png";
+import acilok_img12 from "../../assets/aci.jpg";
 export const product = [
  {
      id:1,
@@ -9,15 +20,69 @@ export const product = [
  },
  {
     id:2,
-    imgPath:abcd_img1,
-    productName:"Paracetamol",
+    imgPath:paaracetamol_img2,
+    productName:"PARACETAMOL",
     price:"30"
  },
  {
     id:3,
-    imgPath:abcd_img1,
-    productName:"ADIronXT",
-    price:"30"
+    imgPath:saridon_img3,
+    productName:"Saridon",
+    price:"40"
+ },
+ {
+    id:4,
+    imgPath:pd_img4,
+    productName:"P.D",
+    price:"237"
+ },
+ {
+    id:5,
+    imgPath:vital_img5,
+    productName:"Vital 40",
+    price:"430"
+ },
+ {
+    id:6,
+    imgPath:zanocin_img6,
+    productName:"Zanocin",
+    price:"560"
+ },
+ {
+    id:7,
+    imgPath:oniron_img7,
+    productName:"Oniron",
+    price:"80"
+ },
+ {
+    id:8,
+    imgPath:cofsils_img8,
+    productName:"Cofsils",
+    price:"100"
+ },
+ {
+    id:9,
+    imgPath:digene_img9,
+    productName:"Digene",
+    price:"340"
+ },
+ {
+    id:10,
+    imgPath:electral_img10,
+    productName:"Electral",
+    price:"40"
+ },
+ {
+    id:11,
+    imgPath:Alkasol_img11,
+    productName:"Alkasol",
+    price:"750"
+ },
+ {
+    id:12,
+    imgPath:acilok_img12,
+    productName:"Aciloc",
+    price:"150"
  }
 ];
 
